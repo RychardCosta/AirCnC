bb84b92a1be5981a04af3874c82502e21466be0e
RychardCosta <55329339+RychardCosta@users.noreply.github.com>
2019-10-13 20:55:33 -0300

    Rychard

---
+++ backend/src/routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
+const DashboardController = require('./controllers/DashboardController');
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const BookingController = require('./controllers/BookingController');
+const ApprovalController = require('./controllers/ApprovalController');
+const RejectController = require('./controllers/RejectController');
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
+
+
+routes.post('/sessions', SessionController.store);
+
+routes.get('/spots', SpotController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
+
+routes.post('/bookings/:booking_id/approvals', ApprovalController.store);
+routes.post('/bookings/:booking_id/rejections', RejectController.store);
+
+
+
+
+module.exports = routes;
